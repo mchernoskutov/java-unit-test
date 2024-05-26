2b5cb35585edc4e09c83e5124ee7c4a43e03ed7f
Chernoskutov Mikhail <mchernoskytovyan@yandex.ru>
2024-05-26 20:57:26 +0100

    Unit Tests

---
+++ task_3.java
@@ -0,0 +1,31 @@
+// Напиши аннотацию для параметризованных тестов
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+	
+  private final int age;
+  private final boolean result;
+
+  public CheckIsAdultTest(int age, boolean result) {
+	this.age = age;
+    this.result = result;  // Инициализируй поля класса в конструкторе
+  }
+
+  @Parameterized.Parameters // Пометь метод аннотацией для параметров
+  public static Object[][] getTextData() {
+	return new Object[][] {
+		{19 , true},
+        {18 , true},
+        {17 , false},
+        {21 , true}// Заполни массив тестовыми данными и ожидаемым результатом
+    };
+  }
+
+  @Test
+  public void checkIsAdultWhenAgeThenResult() {
+	Program program = new Program();
+	 // Передай сюда возраст пользователя
+    boolean isAdult = program.checkIsAdult(age);
+	// Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+    assertEquals("Возраст не соответствует ожидаемому результату",result,isAdult);
+	}
+}
